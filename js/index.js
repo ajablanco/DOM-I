8eb9aa937ce7c94c9cf89e9c8dce5ff06a5afa6f
Aja Blanco <59661417+ajablanco@users.noreply.github.com>
2020-02-10 12:39:36 -0800

    fixed navbar and added cta

--- js/index.js
+++ js/index.js
@@ -38,5 +38,70 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+let header = document.querySelector('header');
+console.log(header);
+header.style.flexDirection = "row";
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let nav = document.querySelector("nav");
+nav.style.width = "70%";
+
+
+const links = document.querySelectorAll('a');
+links.forEach( element => {
+  element.style.color = "lightgrey"; 
+});
+
+console.log(links);
+
+const firstLink = document.querySelector('a:nth-child(1)');
+firstLink.textContent = "Services";
+console.log(firstLink);
+
+const secondLink = document.querySelector('a:nth-child(2)');
+secondLink.textContent = "Product";
+console.log(secondLink);
+
+const thirdLink = document.querySelector('a:nth-child(3)');
+thirdLink.textContent = "Vision";
+console.log(thirdLink);
+
+
+const fourthLink = document.querySelector('a:nth-child(4)');
+fourthLink.textContent = "Features";
+console.log(fourthLink);
+
+const fifthLink = document.querySelector('a:nth-child(5)');
+fifthLink.textContent = "About";
+console.log(fifthLink);
+
+const sixthLink = document.querySelector('a:nth-child(6)');
+sixthLink.textContent = "Contact";
+console.log(sixthLink);
+
+const cta = document.querySelector('.cta');
+
+const ctaHeader = document.querySelector('.cta-text h1');
+ctaHeader.textContent = "DOM";
+ctaHeader.style.width = "400px";
+
+const header2 = document.createElement('h1');
+ctaHeader.append(header2);
+header2.textContent = "is";
+header2.style.width = "400px";
+header2.style.marginBottom = "0";
+
+const header3 = document.createElement('h1');
+ctaHeader.append(header3);
+header3.textContent = "Awesome";
+header3.style.width = "400px";
+header3.style.marginBottom = "0";
+
+const ctaButton = document.querySelector('button');
+ctaButton.textContent = "Get Started";
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = "img/header-img.png";
+console.log(ctaImg);
